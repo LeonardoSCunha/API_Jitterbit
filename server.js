ba5c9e3b71eb870ee8f5f95832026e5758f6d8ec
Leonardo_Cunha <leonardosantoscunha034@gmail.com>
2026-03-08 22:13:05 -0300

    modified:   server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 
 const api_pedido = express()
+api_pedido.use(express.json())
+
+const users = []
+
+api_pedido.post('/user', (request, response) => {
+
+    console.log(request)
+
+    response.send('Cadastrado')
+}
+)
 
 api_pedido.get('/user', (request, response) => {
     return response.send({message: 'ok'})

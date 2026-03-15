eb079fa566552d3c2553610d2949ead4bc869edd
Leonardo_Cunha <leonardosantoscunha034@gmail.com>
2026-03-15 11:40:50 -0300

    new file:   index.html 	modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 const api_pedido = express()
 api_pedido.use(express.json())
+api_pedido.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') return res.sendStatus(204)
+  next()
+})
 
 // Endpoint para criar um novo pedido
 // Recebe orderId, value, creationDate e items no body

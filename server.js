df30895925bb116e2d5ba67f2784ffe2d9a6e2fc
Leonardo_Cunha <leonardosantoscunha034@gmail.com>
2026-03-15 12:51:17 -0300

    fix: update CORS allowed origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 const api_pedido = express()
 api_pedido.use(express.json())
 api_pedido.use((req, res, next) => {
-  const allowed = ['https://69b6cc15f87b4882ca55ef70--api-leonardocunha.netlify.app', 'http://localhost:3000']
+  const allowed = ['https://apiexample-leonardocunha.netlify.app', 'http://localhost:3000']
   const origin = req.headers.origin
   if (allowed.includes(origin)) res.header('Access-Control-Allow-Origin', origin)
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
